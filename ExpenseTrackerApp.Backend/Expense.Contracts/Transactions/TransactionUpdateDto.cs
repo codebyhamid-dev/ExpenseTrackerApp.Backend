a903d90063f1ab82862554b5c5009d99d239cb01
Engr. Hamid Idrees <hamididrees457@gmail.com>
2025-11-14 23:58:40 +0500

    Update TransactionUpdateDto

--- ExpenseTrackerApp.Backend/Expense.Contracts/Transactions/TransactionUpdateDto.cs
+++ ExpenseTrackerApp.Backend/Expense.Contracts/Transactions/TransactionUpdateDto.cs
@@ -3,21 +3,6 @@ using static ExpenseTrackerApp.Backend.Expense.Domain.Enum.AppEnums;
 
 namespace ExpenseTrackerApp.Backend.Expense.Contracts.Transactions
 {
-    public class TransactionUpdateDto
-    {
-        [Required]
-        public Guid UserId { get; set; }
-
-        [Required]
-        public TransactionType TransactionType { get; set; }  // Credit or Debit
-
-        [Required]
-        public PaymentMode PaymentMode { get; set; }          // Cash / Bank / Card
-
-        [Required]
-        public decimal Amount { get; set; }
-
-        public string? Category { get; set; }
-        public string? Description { get; set; }
-    }
+    public class TransactionUpdateDto : TransactionCreateDto { }
+    
 }
